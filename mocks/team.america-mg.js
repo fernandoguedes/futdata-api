96b469226e357ebd11c3ae18cbefc4dd9de5c9bb
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2019-01-16 13:10:43 -0200

    Add mocks to first version

---
+++ mocks/team.america-mg.js
@@ -0,0 +1,13 @@
+module.exports = {
+  general: {
+    name: 'América-MG',
+    normalized: 'america-mg',
+    completeName: 'América Futebol Clube',
+    uf: 'MG',
+    city: 'Belo Horizonte',
+    mascote: 'Coelho',
+    founded: '30/04/1912'
+
+  },
+  lastResults: ['win', 'win', 'draw', 'lost']
+}
---
+++ mocks/teams.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    name: 'América-MG',
+    normalized: 'america-mg',
+  },
+  {
+    name: 'Atlético-MG',
+    normalized: 'atletico-mg'
+  },
+  {
+    name: 'Atlético-PR',
+    normalized: 'atletico-pr'
+  },
+  {
+    name: 'Bahia',
+    normalized: 'bahia'
+  },
+  {
+    name: 'Botafogo',
+    normalized: 'botafogo',
+  },
+  {
+    name: 'Ceara',
+    normalized: 'ceara'
+  },
+  {
+    name: 'Chapecoense',
+    normalized: 'chapecoense'
+  },
+  {
+    name: 'Corinthians',
+    normalized: 'corinthians'
+  },
+  {
+    name: 'Cruzeiro',
+    normalized: 'cruzeiro'
+  },
+  {
+    name: 'Flamengo',
+    normalized: 'flamengo',
+  },
+  {
+    name: 'Fluminense',
+    normalized: 'fluminense'
+  },
+  {
+    name: 'Grêmio',
+    normalized: 'gremio'
+  },
+  {
+    name: 'Internacional',
+    normalized: 'internacional'
+  },
+  {
+    name: 'Palmeiras',
+    normalized: 'palmeiras'
+  },
+  {
+    name: 'Paraná Clube',
+    normalized: 'parana-clube',
+  },
+  {
+    name: 'Santos',
+    normalized: 'santos'
+  },
+  {
+    name: 'São Paulo',
+    normalized: 'sao-paulo'
+  },
+  {
+    name: 'Sport',
+    normalized: 'sport'
+  },
+  {
+    name: 'Vasco',
+    normalized: 'vasco'
+  },
+  {
+    name: 'Vitória',
+    normalized: 'vitoria'
+  }
+]
